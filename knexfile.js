7d7db5b0947e218b7c1df040adf43626b63cf604
Nicola Dall'Asen <nicola.dallasen@gmail.com>
2019-05-03 14:13:21 +0200

    knex files

---
+++ knexfile.js
@@ -0,0 +1,32 @@
+module.exports = {
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/test_db',
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/test'
+    }
+  },
+  development: {
+    client: 'pg',
+    connection: 'postgres://postgres:@localhost:32768/development_db',
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/development'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/production'
+    }
+  }
+}
